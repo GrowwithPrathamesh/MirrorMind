dfa6ae4f702ca3339dc2bd1ecd9a93879ba23212
Prathamesh Rahate <growwithprathamesh@gmail.com>
2025-12-28 22:09:40 +0530

    attendence

--- MirrorMind/settings.py
+++ MirrorMind/settings.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Application definition
 
@@ -39,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Accounts',
+    'rest_framework',
+    'Attendance',
 ]
 
 MIDDLEWARE = [
