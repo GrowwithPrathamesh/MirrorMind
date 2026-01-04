7633c79b965fa57e9ae62fa684dfa7f6563556b8
Prathamesh Rahate <growwithprathamesh@gmail.com>
2026-01-04 11:33:08 +0530

    dfd

--- MirrorMind/urls.py
+++ MirrorMind/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('student-signup/', views.student_signup),
     path('teacher-signup/', views.teacher_signup),
     path("email-otp/", views.email_otp_handler),
-    path("student-forgot-password/", views.student_forgot_password),
-    path("teacher-forgot-password/", views.teacher_forgot_password),
+    path("student-reset-password/", views.student_reset_password),
+    path("teacher-reset-password/", views.teacher_reset_password),
     path("student-login/", views.student_login),
     path("teacher-login/", views.teacher_login),
 ]
